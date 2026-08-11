e0f136e80c743572c7c6c8eb79308db1311be604
Maropeng Mashau <ST10498486@rcconnect.edu.za>
2026-08-12 00:57:07 +0200

    Introduction ,Labels.
    First part of Project. Added ascii art labels, txt readFile, and name collection.
    Created a BotAwaken.cs class that will hold methods and if statements

---
+++ ConsoleApp1/ConsoleApp1/BotAwaken.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BotAwaken
+    {
+        bool running = true;
+        public BotAwaken()
+        {
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("bot");
+            Console.ResetColor();
+            Console.WriteLine(": Hi , what is your name?");
+            Console.Write("> ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("");
+            Console.WriteLine("*****************************");
+            Console.WriteLine($"*  User: {name}            *");
+            Console.WriteLine("*****************************");
+            Console.WriteLine("");
+            string filePath = "art.txt";
+            string art = File.ReadAllText(filePath);
+            Console.WriteLine(art);
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>----<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>----<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--\r\n");
+            Console.ResetColor();
+
+
+            Console.WriteLine($": Nice to meet you {name}.I am CSCB, a Cyber Security awareness ChatBot. You cana ask me anything about cyber security.");
+            Console.WriteLine("   Or say exit to end the program.");
+            //
+            //Whiel loop and if-statements here + methods
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>----<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>----<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--<<O>>--\r\n");
+            Console.ResetColor();
+            string label = @" ██████ ███████  ██████ ██████                 ██████ ██    ██ ██████  ███████ ██████      ███████ ███████  ██████ ██    ██ ██████  ██ ████████ ██    ██      █████  ██     ██  █████  ██████  ███████ ███    ██ ███████ ███████ ███████ 
+██      ██      ██      ██   ██               ██       ██  ██  ██   ██ ██      ██   ██     ██      ██      ██      ██    ██ ██   ██ ██    ██     ██  ██      ██   ██ ██     ██ ██   ██ ██   ██ ██      ████   ██ ██      ██      ██      
+██      ███████ ██      ██████      █████     ██        ████   ██████  █████   ██████      ███████ █████   ██      ██    ██ ██████  ██    ██      ████       ███████ ██  █  ██ ███████ ██████  █████   ██ ██  ██ █████   ███████ ███████ 
+██           ██ ██      ██   ██               ██         ██    ██   ██ ██      ██   ██          ██ ██      ██      ██    ██ ██   ██ ██    ██       ██        ██   ██ ██ ███ ██ ██   ██ ██   ██ ██      ██  ██ ██ ██           ██      ██ 
+ ██████ ███████  ██████ ██████                 ██████    ██    ██████  ███████ ██   ██     ███████ ███████  ██████  ██████  ██   ██ ██    ██       ██        ██   ██  ███ ███  ██   ██ ██   ██ ███████ ██   ████ ███████ ███████ ███████ 
+                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                         ";
+
+            Console.WriteLine(label);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Welcome to the Cyber Security Awareness Chatbot. Here to help you stay safe online.");
+            Console.WriteLine("Say hi or hello to wake the bot");
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            if (choice.ToLower().Contains("hi") || choice.ToLower().Contains("hello")) {
+                wakeBot();
+            }
+        }
+        static void wakeBot()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Bot is Awake!");
+            Console.ResetColor();
+            BotAwaken ba = new BotAwaken();
+            
         }
     }
 }
