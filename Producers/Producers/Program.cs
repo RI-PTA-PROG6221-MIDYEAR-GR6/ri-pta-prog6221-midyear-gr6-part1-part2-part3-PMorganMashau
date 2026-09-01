791619cc97b9844871537eb5bac62310a8d258f7
Maropeng Mashau <ST10498486@rcconnect.edu.za>
2026-09-01 14:46:52 +0200

    Added responses to hi, hello,how are you

---
+++ Producers/Producers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Media;
+
+SoundPlayer sound = new SoundPlayer("sound.wav");
+sound.Play();
+
+Console.WriteLine("Plaay sound?(Y/N)");
+string ans = Console.ReadLine();
+
+if (ans.ToUpper() == "Y")
+{
+    Console.WriteLine("Playing sound...");
+    sound.Play();
+}
+else
+{
+    Console.WriteLine("Sound not played.");
+}
